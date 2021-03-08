cbf65d5632942b315e411652d93084144d4e6d0b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-03-08 15:52:59 +0000

    Add `google-beta` provider, move `terraform` block to convetional `versions.tf`

--- provider_google.tf
+++ provider_google.tf
@@ -1,15 +1,10 @@
-terraform {
-  required_version = ">= 0.14.0"
-
-  required_providers {
-    google = {
-      version = "~> 3.55.0"
-      source = "hashicorp/google"
-    }
-  }
+provider google {
+  credentials = file("./secrets/service-account.${var.svc_account_key}.key.json")
+  project     = var.project_id
+  region      = var.cluster_location
 }
 
-provider google {
+provider google-beta {
   credentials = file("./secrets/service-account.${var.svc_account_key}.key.json")
   project     = var.project_id
   region      = var.cluster_location
